bc91005b0d3a42cec8f45bae35d60b60693064c2
dulcoral <dulcoral20@gmail.com>
2017-06-21 13:44:58 -0500

    actividad vibrar

--- Sensores/app/src/main/java/com/example/rodrigo/sensores/VibradorActivity.java
+++ Sensores/app/src/main/java/com/example/rodrigo/sensores/VibradorActivity.java
@@ -7,5 +7,68 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class VibradorActivity extends AppCompatActivity{
+   Button unaV,repetidas,stop;
+    boolean estaVibrando = false;
+    Vibrator vibrador;
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vibracion);
+        unaV= (Button) findViewById(R.id.vibrarUna);
+        repetidas= (Button) findViewById(R.id.vibrarRepetir);
+        stop= (Button) findViewById(R.id.stop);
+        vibrador = ((Vibrator) getSystemService(VIBRATOR_SERVICE));
+        vibrador.cancel();
+    }
+    public void vibrarUnaVez (View view){
+        if(!vibrador.hasVibrator()){
+            Toast.makeText(VibracionActivity.this, "No soporta vibracion",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(estaVibrando)
+            vibrador.cancel();
+        vibrador.vibrate(1000);
+        estaVibrando=true;
+        stop.setEnabled(true);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                estaVibrando= false;
+                stop.setEnabled(false);
+            }
+        },1000);
+
+    }
+
+    public void vibrarRepetidas (View view){
+        if(!vibrador.hasVibrator()){
+            Toast.makeText(VibracionActivity.this, "No soporta vibracion",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(estaVibrando)
+            vibrador.cancel();
+        long delay = 250;
+        long sigueVip = 250;
+        long on_off=250;
+        int repeat =1;
+
+        vibrador.vibrate(new long[]{delay,sigueVip,on_off,on_off,on_off},repeat);
+        estaVibrando=true;
+        stop.setEnabled(true);
+    }
+
+    public void detener(View view){
+        if(!vibrador.hasVibrator()){
+            Toast.makeText(VibracionActivity.this, "No soporta vibracion",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(estaVibrando){
+            vibrador.cancel();
+            stop.setEnabled(false);
+            estaVibrando= false;
+        }
+    }
+
 
 }
